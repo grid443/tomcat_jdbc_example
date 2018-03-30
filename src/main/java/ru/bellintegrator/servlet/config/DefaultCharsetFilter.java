9a7b9435b631c5efb80d1511c6aa2153c379df32
Кирилл Васин <v.kirill.01@gmail.com>
2018-03-31 00:35:08 +0300

    format

--- src/main/java/ru/bellintegrator/servlet/config/DefaultCharsetFilter.java
+++ src/main/java/ru/bellintegrator/servlet/config/DefaultCharsetFilter.java
@@ -21,8 +21,11 @@ public class DefaultCharsetFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain
+    ) throws IOException, ServletException {
         request.setCharacterEncoding(DEFAULT_CHARSET);
 
         response.setCharacterEncoding(DEFAULT_CHARSET);
